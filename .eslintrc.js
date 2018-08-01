8d9595db5e08bca02b11660429354f6c59c35e98
Dmitrij Borovskij <dmitr.borovskij@gmail.com>
2018-08-01 17:45:05 +0300

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,56 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es6': true
+  },
+  'extends': 'airbnb',
+  'parser': 'babel-eslint',
+  'parserOptions': {
+    'ecmaFeatures': {
+      'experimentalObjectRestSpread': true,
+      'jsx': true,
+      'es6': true,
+    },
+    'sourceType': 'module'
+  },
+  'plugins': [
+    'babel',
+    'react',
+    'jsx-a11y',
+    'import'
+  ],
+  'rules': {
+    'no-tabs': 0,
+    'indent': ['error', 'tab', { 'SwitchCase': 1 }],
+    'semi': ['error', 'never'],
+    'max-len': ['error', { 'code': 120, 'ignoreUrls': true }],
+    'no-console': ['warn'],
+    'no-unused-vars': ['warn'],
+    'consistent-return': 0,
+    'comma-dangle': ['error', 'never'],
+    'linebreak-style': ['error', 'unix'],
+    'quotes': ['error', 'single'],
+    'arrow-parens': ['error', 'as-needed'],
+    'arrow-body-style': 0,
+    'react/prop-types': 0,
+    'react/jsx-indent': [2, 'tab'],
+    'jsx-quotes': ['error', 'prefer-single'],
+    'jsx-a11y/anchor-is-valid': [ 'error', {
+      'components': [ 'Link' ],
+      'specialLink': [ 'to' ],
+      'aspects': [ 'noHref' ]
+    }],
+    'jsx-a11y/no-redundant-roles': 0,
+    'object-curly-newline': 0,
+    'import/prefer-default-export': 0,
+    'react/jsx-closing-bracket-location': 1,
+    'react/jsx-curly-spacing': ['error', 'always'],
+    'react/jsx-curly-brace-presence': 0,
+    'react/prefer-stateless-function': ['error', { 'ignorePureComponents': true }],
+    'react/jsx-filename-extension': [1, { 'extensions': ['.js', '.jsx'] }],
+    'react/jsx-indent-props': ['error', 'tab'],
+    'react/jsx-no-undef': 1,
+    'react/prefer-es6-class': 1,
+    'react/jsx-no-bind': 1,
+  },
+};
--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import styles from './App.css';
+import React from 'react'
+import logo from './logo.svg'
+import styles from './App.css'
 
-class App extends Component {
-  render() {
-    return (
-      <div className={styles.app}>
-        <header className={styles.header}>
-          <img src={logo} className={styles.logo} alt="logo" />
-          <h1 className={styles.title}>Welcome to React</h1>
-        </header>
-        <p className={styles.intro}>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => (
+	<div className={ styles.app }>
+		<header className={ styles.header }>
+			<img src={ logo } className={ styles.logo } alt='logo' />
+		</header>
+	</div>
+)
 
-export default App;
+export default App
